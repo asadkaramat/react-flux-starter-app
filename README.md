93d8b7de711c9c883d42ce4739210456b038778d
Asad Ullah <Asad Ullah>
2017-01-28 00:09:11 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+React with flux starter kit.
+To get started:
+
+* Install NodeJS
+* Download this repository
+* Open this directory in your command line and type
+* npm install - Installs packages
+* npm install -g gulp
+* gulp - to start
+* Navigate to http://localhost:9006/ 
---
+++ src/components/homePage.js
@@ -0,0 +1,14 @@
+"use strict";
+var React = require('react');
+
+var Home = React.createClass({
+    render: function() {
+        return (
+            <div className="jumbotron">
+                <h1> Hello world</h1>
+            </div>
+        );
+    }
+});
+
+module.exports = Home;
